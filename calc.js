4c67f29d06312251ba6d9db6d3c7b24553ec6fd7
= <=>
2021-01-09 02:16:02 +0200

    Beautified code and updated variables(more refactoring incoming FeelsGoodMan)

--- calc.js
+++ calc.js
@@ -15,7 +15,7 @@ var coordinates = {
 var unitInterval = {
     base: 0,
     exponent: 0, //10 is squared by this exponent property
-    count: { 
+    count: {
         initialValue: 10,
         value: 10, //based on z's fraction value
         rateOfChange: 1.5
@@ -30,7 +30,7 @@ var windowDimensions = {
 
 var canvas = {
     htmlElement: document.querySelector('#canvas'),
-    getCtx:() => {
+    getCtx: () => {
         var htmlElement = document.querySelector('#canvas');
         return htmlElement.getContext('2d');
     }
@@ -62,10 +62,10 @@ function dragEvent(e) {
     document.onmousemove = (e) => { //drag party of drag event ;)
         posXDifference = e.clientX - posX;
         posYDifference = e.clientY - posY;
-        if(Math.abs(posXDifference) + Math.abs(posYDifference) > 5) { //makes rerendering less frequent to improve performance
+        if (Math.abs(posXDifference) + Math.abs(posYDifference) > 5) { //makes rerendering less frequent to improve performance
             var currentCoordinates = calculateCoordinatesFromPx(e.clientX, e.clientY); //get current coordinates
             var prevCoordinates = calculateCoordinatesFromPx(posX, posY); //get previousCoordinates
-            if(currentCoordinates != null && prevCoordinates != null) {
+            if (currentCoordinates != null && prevCoordinates != null) {
                 calculateDragNewMdpt(prevCoordinates[1], currentCoordinates[1], prevCoordinates[0], currentCoordinates[0]);
             }
             renderGrid();
@@ -78,66 +78,61 @@ function renderGrid() {
     setCanvasDimensions();
     clearCanvas();
     const tenthPower = 10 ** unitInterval.exponent
-    const unit = unitInterval.base * tenthPower;
-    const roundedX = intToIntRounder(coordinates.x, unit);
-    const roundedY =  intToIntRounder(coordinates.y, unit);
+    const interval = unitInterval.base * tenthPower;
+    const roundedX = roundToInterval(coordinates.x, interval);
+    const roundedY = roundToInterval(coordinates.y, interval);
     const xOffset = (roundedX - coordinates.x) + coordinates.x;
     const yOffset = (roundedY - coordinates.y) + coordinates.y;
     var i = 0;
-    while(i <= (unitInterval.count.value / 2) + 1) {
-        renderXAxis(i, xOffset, unit);
-        renderYAxis(i, yOffset, unit);
+    while (i <= (unitInterval.count.value / 2) + 1) {
+        renderXAxis(i, xOffset, interval);
+        renderYAxis(i, yOffset, interval);
         i++;
     }
 }
 
-function renderXAxis(index, offset, unit) {
-    var x = offset + (unit * index); //positive x value(s)
+function renderXAxis(index, offset, interval) {
+    var x = offset + (interval * index); //positive x value(s)
     var xPositiveCoordinates = calculatePxFromCoordinates(x, coordinates.y);
-    x = offset - (unit * index); //negative x value(s)
+    x = offset - (interval * index); //negative x value(s)
     var xNegativeCoordinates = calculatePxFromCoordinates(x, coordinates.y);
-    if(xPositiveCoordinates == null || index == 0) {
+    if (xPositiveCoordinates == null || index == 0) {
         //do nothing
-    } 
-    else {
+    } else {
         drawLine(xPositiveCoordinates[0], xPositiveCoordinates[0], 0, windowDimensions.height, 1, 'black');
     }
-    if(xNegativeCoordinates == null) {
+    if (xNegativeCoordinates == null) {
         //do nothing   
-    }
-    else {
+    } else {
         drawLine(xNegativeCoordinates[0], xNegativeCoordinates[0], 0, windowDimensions.height, 1, 'black');
     }
 }
 
-function renderYAxis(index, offset, unit) {
-    var y = offset + (unit * index); //positive x value(s)
+function renderYAxis(index, offset, interval) {
+    var y = offset + (interval * index); //positive x value(s)
     var yPositiveCoordinates = calculatePxFromCoordinates(coordinates.x, y);
-    y = offset - (unit * index); //negative x value(s)
+    y = offset - (interval * index); //negative x value(s)
     var yNegativeCoordinates = calculatePxFromCoordinates(coordinates.x, y);
-    if(yPositiveCoordinates == null || index == 0) {
+    if (yPositiveCoordinates == null || index == 0) {
         //do nothing
-    } 
-    else {
-        drawLine(0, windowDimensions.width, yPositiveCoordinates[1] , yPositiveCoordinates[1], 1, 'black');
+    } else {
+        drawLine(0, windowDimensions.width, yPositiveCoordinates[1], yPositiveCoordinates[1], 1, 'black');
     }
-    if(yNegativeCoordinates == null) {
+    if (yNegativeCoordinates == null) {
         //do nothing   
-    }
-    else {
-        drawLine(0, windowDimensions.width, yNegativeCoordinates[1] , yNegativeCoordinates[1], 1, 'black');
+    } else {
+        drawLine(0, windowDimensions.width, yNegativeCoordinates[1], yNegativeCoordinates[1], 1, 'black');
     }
 }
 
 function calculateCoordinatesFromPx(x, y) {
     var mdptX = windowDimensions.width / 2;
     var mdptY = windowDimensions.height / 2;
-    if(Math.sign(x) == -1 || Math.sign(y) == -1 || x > windowDimensions.width || y > windowDimensions.height ) {
+    if (Math.sign(x) == -1 || Math.sign(y) == -1 || x > windowDimensions.width || y > windowDimensions.height) {
         return null;
-    }
-    else {
+    } else {
         x = ((x - mdptX) / unitInterval.pxPerUnit) + coordinates.x; //moves x's 0 value from top left to center
-        y = (((y * - 1) + mdptY) / unitInterval.pxPerUnit) + coordinates.y; //moves y's 0 value from top left to center
+        y = (((y * -1) + mdptY) / unitInterval.pxPerUnit) + coordinates.y; //moves y's 0 value from top left to center
 
         return [x, y];
     }
@@ -150,10 +145,9 @@ function calculatePxFromCoordinates(x, y) {
     xPx = ((x - coordinates.x) * unitInterval.pxPerUnit) + mdptX; //pixels can't be negative, must be cast to positive number. 
     yPx = (((y - coordinates.y) * unitInterval.pxPerUnit) - mdptY) * -1; //pixels can't be negative, must be cast to positive number.
 
-    if(Math.sign(xPx) == -1 || Math.sign(yPx) == -1  || xPx > windowDimensions.width || yPx > windowDimensions.height) {
+    if (Math.sign(xPx) == -1 || Math.sign(yPx) == -1 || xPx > windowDimensions.width || yPx > windowDimensions.height) {
         return null;
-    }
-    else {
+    } else {
         return [xPx, yPx];
     }
 }
@@ -170,12 +164,11 @@ function calculateDragNewMdpt(y2, y1, x2, x1) {
 //3.calculate the base of the interval based on remainder
 //4.If remainder is 0 or 2 calculate the exponent of the interval
 //5.Set pxPerUnit value(VERY IMPORTANT!!!)
-document.addEventListener('wheel', (e) => { 
-    if(e.deltaY == 100) { //deltaY = 100 if mouseWheelUp; deltaY = -100 if mouseWheelDown
+document.addEventListener('wheel', (e) => {
+    if (e.deltaY == 100) { //deltaY = 100 if mouseWheelUp; deltaY = -100 if mouseWheelDown
         coordinates.z.fraction++;
         handleZFractionLoop();
-    }
-    else {
+    } else {
         coordinates.z.fraction--;
         handleZFractionLoop();
     }
@@ -185,12 +178,11 @@ document.addEventListener('wheel', (e) => {
 });
 
 function handleZFractionLoop() {
-    if(coordinates.z.fraction == 10) { //handles zoom out looping
+    if (coordinates.z.fraction == 10) { //handles zoom out looping
         coordinates.z.base++;
         coordinates.z.fraction = 0;
         calculateIntervalBase();
-    }
-    else if(coordinates.z.fraction == -1) { //handles zoom in looping
+    } else if (coordinates.z.fraction == -1) { //handles zoom in looping
         coordinates.z.base--;
         coordinates.z.fraction = 9;
         calculateIntervalBase();
@@ -214,14 +206,14 @@ function calculateIntervalBase() {
             break;
         default:
             //do nothings
-    } 
+    }
 }
 
 function calculateIntervalExponent(remainder) {
     //remainder is minused to "round" to the 1st factor of 3 below the current base.
     //the case above is only necessary when base is moving from 1 -> 5
     var defaultZDifference = coordinates.z.base - coordinates.z.initialBase - remainder;
-    unitInterval.exponent = defaultZDifference / 3; 
+    unitInterval.exponent = defaultZDifference / 3;
 }
 
 function setCanvasDimensions() {
@@ -251,37 +243,35 @@ function clearCanvas() {
     ctx.clearRect(0, 0, windowDimensions.width, windowDimensions.height); //easy way to clear canvas.
 }
 
-function intToIntRounder(num, rounder) {
-    if(Math.sign(rounder) == -1) {
+function roundToInterval(num, interval) {
+    if (Math.sign(interval) == -1) {
         return num;
     }
     var tempExponent = null; //variable used if exponent is negative
-    var remainder = num % rounder;
-    if(Math.sign(remainder) == -1 && Math.abs(remainder) < rounder / 2) {
+    var remainder = num % interval;
+    if (Math.sign(remainder) == -1 && Math.abs(remainder) < interval / 2) { //handles negative signs which is inverted for math.ceil and math.floor(i don't want the inverted thingies)
         remainder = 10 - remainder;
     }
-    if(Math.sign(unitInterval.exponent) == -1) {
+    if (interval < 1) { //handles floats by rounding interval and num up to integer 
         tempExponent = unitInterval.exponent * -1;
         num = num * 10 ** tempExponent;
-        rounder = rounder * 10 ** tempExponent;
+        interval = interval * 10 ** tempExponent;
     }
-    if(remainder < rounder / 2){
-        while(num % rounder != 0) {
+    if (remainder < interval / 2) {
+        while (num % interval != 0) {
             num = Math.floor(num - 0.01);
         }
-        if(tempExponent != null) {
-            return num * 10 ** unitInterval.exponent;
+        if (tempExponent != null) {
+            return num * 10 ** unitInterval.exponent; //float(integer now) taken back to float after being rounded
         }
         return num;
-    }
-    else {
-        while(num % rounder != 0) {
+    } else {
+        while (num % interval != 0) {
             num = Math.ceil(num + 0.01);
         }
-        if(tempExponent != null) {
-            return num * 10 ** unitInterval.exponent;
+        if (tempExponent != null) {
+            return num * 10 ** unitInterval.exponent; //float(integer now) taken back to float after being rounded
         }
         return num;
     }
 }
-
